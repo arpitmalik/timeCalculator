80d0245e385b2ae5532dc79cb87efcb4692ffb34
Arpit Malik <arpitmalik04@gmail.com>
2024-11-14 17:32:15 +0530

    Added Offline Support

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,43 @@
-// service-worker.js
-const CACHE_NAME = 'time-calculator-v1';
+const CACHE_NAME = "time-calculator-cache-v1";
 const urlsToCache = [
-  './',
-  './index.html'
-  // Add other assets like images, fonts, etc., if you have any
+  "/",
+  "/index.html",
+  "/service-worker.js",
+  "/manifest.json",
+  "/style.css", // Add other files if you have more assets
 ];
 
-self.addEventListener('install', event => {
+// Install service worker and cache assets
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
+// Fetch resources from cache if available
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
 
-self.addEventListener('activate', event => {
+// Update cache if there are any changes to assets
+self.addEventListener("activate", (event) => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
           if (!cacheWhitelist.includes(cacheName)) {
             return caches.delete(cacheName);
           }
         })
-      );
-    })
+      )
+    )
   );
 });
